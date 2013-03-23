85f7459f990a7ed476fed77bfd455c56b3f81c26
Thorsten Lorenz <thlorenz@gmx.de>
2013-03-23 15:03:22 -0400

    working compile function returns source, maps and error if present

---
+++ compile.js
@@ -0,0 +1,65 @@
+'use strict';
+
+var nodeTraceur        =  require('node-traceur')
+  , traceur            =  nodeTraceur.traceur
+  , compile            =  traceur.codegeneration.Compiler.compile
+  , SourceMapGenerator =  traceur.outputgeneration.SourceMapGenerator
+  , Project            =  traceur.semantics.symbols.Project
+  , ProjectWriter      =  traceur.outputgeneration.ProjectWriter
+  , SourceFile         =  traceur.syntax.SourceFile
+  , format             =  require('util').format
+  , path               =  require('path')
+  ;
+
++function initGlobalTraceurOptions() {
+  [ 'arrayComprehension'
+  , 'arrowFunctions'
+  , 'classes'
+  , 'defaultParameters'
+  , 'destructuring'
+  , 'forOf'
+  , 'propertyMethods'
+  , 'propertyNameShorthand'
+  , 'templateLiterals'
+  , 'restParameters'
+  , 'spread'
+  , 'generatorComprehension'
+  , 'generators'
+  , 'modules'
+  , 'blockBinding'
+  , 'sourceMaps'
+  ].forEach(function (k) { traceur.options[k] = true; });
+}();
+
+module.exports = function compileFile(file, contents) {
+
+  var name       =  path.basename(file)
+    , project    =  new Project(file)
+    , sourceFile =  new SourceFile(name, contents)
+    , err
+    ;
+
+  var reporter = {
+    reportError : function(pos, msg) {
+      err = format('%s:%s:%s %s', file, pos.line, pos.offset, msg);
+    },
+    hadError : function () { return !!err; } 
+  };
+
+  project.addFile(sourceFile);
+
+  var compiled = compile(reporter, project, false); 
+
+  if (err) return { source: null, sourcemap: null, error: err };
+
+  var options;
+
+  if (traceur.options.sourceMaps) {
+    var config = { file: file + '.es6' };
+    var sourceMapGenerator = new SourceMapGenerator(config);
+    options = { sourceMapGenerator: sourceMapGenerator };
+  }
+
+  var source = ProjectWriter.write(compiled, options);
+  return { source: source, sourcemap: options.sourceMap, error: null };
+};
