cc0cbc98ed1daf6e26a2ada2d62c474d527c8486
Thorsten Lorenz <thlorenz@gmx.de>
2013-03-24 11:49:53 -0400

    0.2.0 adding caching and renaming transform to configure

--- test/transform.js
+++ test/transform.js
@@ -1,27 +1,36 @@
 'use strict';
 /*jshint asi: true */
 
-var test = require('tap').test
-
-var fs        =  require('fs');
-var path      =  require('path');
-var through   =  require('through');
-var convert   =  require('convert-source-map');
-var es6ify    =  require('..');
+var test       =  require('tap').test
+  , fs         =  require('fs')
+  , path       =  require('path')
+  , through    =  require('through')
+  , convert    =  require('convert-source-map')
+  , compile    =  require('../compile')
+  , proxyquire =  require('proxyquire')
 
 test('transform adds sourcemap comment and uses cache on second time', function (t) {
-    t.plan(1);
+
+    t.plan(2);
     var data = '';
     var compiles = 0;
-//    var originalCompile = coffee.compile;
 
-    /*coffee.compile = function () {
+    function trackingCompile() {
       compiles++;
       var args = [].slice.call(arguments);
-      return originalCompile.apply(this, args);
-    };*/
+      return compile.apply(this, args);
+    }
+
+    var es6ify = proxyquire('..', { './compile' : trackingCompile } )
 
     var file = path.join(__dirname, '../example/src/features/iterators.js');
+
+    // first time
+    fs.createReadStream(file)
+        .pipe(es6ify(file))
+        .pipe(through(write));
+    
+    // second time
     fs.createReadStream(file)
         .pipe(es6ify(file))
         .pipe(through(write, end));
@@ -40,5 +49,6 @@ test('transform adds sourcemap comment and uses cache on second time', function 
             sourcesContent: [ 'module.exports = function () {\n  for (let element of [1, 2, 3]) {\n    console.log(\'element:\', element);\n  }\n};\n' ] }
         , 'adds sourcemap comment including original source'
       );
+      t.equal(compiles, 1, 'compiles only the first time');
     }
 });
