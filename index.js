f83e0151cc41b9bb211609bba4550047c8c9a4c7
ForbesLindesay <forbes@lindesay.co.uk>
2013-08-25 13:03:49 +0100

    Expose `compileFile` as a public API method
    This module is very useful for things way outside of browserify.
    Outside of browserify, the streaming is usually a pain and unnecessary
    (given that it buffers anyway).  This exposes a synchronous, buffered
    API.

--- index.js
+++ index.js
@@ -61,3 +61,4 @@ function es6ify(filePattern) {
 module.exports           =  es6ify();
 module.exports.configure =  es6ify;
 module.exports.runtime   =  require.resolve('traceur/src/runtime/runtime.js');
+module.exports.compileFile = compileFile;
