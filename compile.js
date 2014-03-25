13abc7e9299bc009cd9d5d1c775dc21457ae73de
Bjørge Næss <bjoerge@gmail.com>
2014-03-25 19:00:30 +0100

    Remove hard coded traceur options

--- compile.js
+++ compile.js
@@ -1,35 +1,15 @@
 'use strict';
 
-var extend = require("util-extend");
 var compile = require("traceur").compile;
-
-var options = {
-    experimental: true,
-    arrayComprehension: true,
-    arrowFunctions: true,
-    classes: true,
-    computedPropertyNames: true,
-    defaultParameters: true,
-    destructuring: true,
-    forOf: true,
-    generatorComprehension: true,
-    generators: true,
-    modules: 'commonjs',
-    numericLiterals: true,
-    propertyMethods: true,
-    propertyNameShorthand: true,
-    restParameters: true,
-    spread: true,
-    templateLiterals: true,
-    blockBinding: true,
-    symbols: true,
-    deferredFunctions: true,
-    sourceMap: true
-};
 
 module.exports = function compileFile(file, contents) {
 
-    var result = compile(contents, extend(options, { filename: file }));
+    var result = compile(contents, { 
+      modules: 'commonjs',
+      blockBinding: true,
+      sourceMap: true,
+      filename: file
+    });
 
     if (result.errors.length) {
         return { source: null, sourcemap: null, error: result.errors[0] };
