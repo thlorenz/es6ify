c1dfe06f3ecba29ec8c8e0706f829ddbab23d4a5
Thorsten Lorenz <thlorenz@gmx.de>
2014-03-27 18:25:30 -0400

    upgrade browserify and fix bundle tests
    - each test now denotes via a flag if it needs the traceur runtime or not

--- test/bundle.js
+++ test/bundle.js
@@ -9,29 +9,33 @@ var format     =  require('util').format;
 
 [ [ 'run-destructuring'     , [ 'hello, world' ] ]
 , [ 'run-block-scope'       , [ 'tmp is undefined:  true' ] ]
-, [ 'run-classes'       , [ 'An instance of Foo says hi from its .toString()!' ] ]
 , [ 'run-default-parameters', [ 'name: Bruno, codes: JavaScript, lives in: USA' ] ]
 , [ 'run-rest-parameters'   , ['list fruits has the following items', 'apple', 'banana' ] ]
-, [ 'run-spread-operator'       , [ '3 + 4 = 7' ] ]
-, [ 'run-combined', [
-      'hello, world'
+, [ 'run-classes'       , [ 'An instance of Foo says hi from its .toString()!' ], true ]
+, [ 'run-spread-operator'       , [ '3 + 4 = 7' ], true ]
+, [ 'run-combined'
+  , [ 'hello, world'
     , 'tmp is undefined:  true'
     , 'An instance of Foo says hi from its .toString()!'
     , 'name: Bruno, codes: JavaScript, lives in: USA'
     , 'list fruits has the following items', 'apple', 'banana' 
     , '3 + 4 = 7'
     ] 
+  , true
   ] 
 ].forEach(function (row) {
 
-  var filename     =  row[0];
-  var expectedLogs =  row[1];
+  var filename     = row[0];
+  var expectedLogs = row[1];
+  var useRuntime   = !!row[2];
 
-  test('\nbundle with runtime - ' + filename, function (t) {
+  test('\nbundle ' + (useRuntime ? 'with' : 'without') + ' traceur runtime - ' + filename, function (t) {
     t.plan(expectedLogs.length)
       
-    browserify()
-      .add(es6ify.runtime)
+    var bfy = browserify();
+    if (useRuntime) bfy.add(es6ify.runtime);
+
+    bfy
       .transform(es6ify)
       .require(__dirname + '/bundle/' + filename + '.js', { entry: true })
       .bundle(function (err, src) {
