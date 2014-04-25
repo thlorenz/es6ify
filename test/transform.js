cf06640425b9bd8e65cbaab6974eb3f631c42e0a
Arturo Guzman <arturo@kisurfer.com>
2014-04-24 23:26:16 -0400

    add transform test file

---
+++ test/transform.js
@@ -0,0 +1,57 @@
+'use strict';
+/*jshint asi: true */
+
+var test       =  require('tap').test
+  , fs         =  require('fs')
+  , path       =  require('path')
+  , through    =  require('through')
+  , convert    =  require('convert-source-map')
+  , compile    =  require('../compile')
+  , proxyquire =  require('proxyquire')
+
+test('transform adds sourcemap comment and uses cache on second time', function (t) {
+
+    t.plan(3);
+    var data = '';
+    var compiles = 0;
+
+    function trackingCompile() {
+      compiles++;
+      var args = [].slice.call(arguments);
+      return compile.apply(this, args);
+    }
+
+    var es6ify = proxyquire('..', { './compile' : trackingCompile } )
+    var es6ifyTransform = es6ify({ traceurOverrides: { blockBinding: true } });
+
+    var file = path.join(__dirname, '../example/src/features/iterators.js');
+
+    // first time
+    fs.createReadStream(file)
+      .pipe(es6ifyTransform(file))
+      .on('error', console.error)
+      .pipe(through(write));
+
+    // second time
+    fs.createReadStream(file)
+      .pipe(es6ifyTransform(file))
+      .on('error', console.error)
+      .pipe(through(write, end));
+
+    function write (buf) { data += buf; }
+    function end () {
+      var sourceMap = convert.fromSource(data).toObject();
+      t.deepEqual(
+          sourceMap
+        , { version: 3,
+            file: file + '.es6',
+            sources: [ file ],
+            names: [],
+            mappings: sourceMap.mappings,
+            sourcesContent: [ 'module.exports = function () {\n  for (let element of [1, 2, 3]) {\n    console.log(\'element:\', element);\n  }\n};\n' ] }
+        , 'adds sourcemap comment including original source'
+      );
+      t.ok(sourceMap.mappings.length);
+      t.equal(compiles, 1, 'compiles only the first time');
+    }
+});
