19f88696cce1590dacfa97fb082074b7d9262cf7
Arturo Guzman <arturo@kisurfer.com>
2014-04-24 23:35:01 -0400

    add errors test which is the same as the compat since the new api supports the pure transform function

---
+++ test/errors.js
@@ -0,0 +1,27 @@
+'use strict';
+/*jshint asi: true */
+
+var test       =  require('tap').test;
+var browserify =  require('browserify');
+var es6ify     =  require('..');
+var path       =  require('path');
+
+test('\nsyntax error', function (t) {
+  browserify()
+    .transform(es6ify)
+    .require(__dirname + '/bundle/syntax-error.js', { entry: true })
+    .bundle(function (err, src) {
+      t.equal(err.message, path.resolve(__dirname + '/bundle/syntax-error.js') + ':1:10: \'identifier\' expected');
+      t.end();
+    });
+})
+
+test('\ncompiling block-scope without blockBinding override', function (t) {
+  browserify()
+    .transform(es6ify)
+    .require(__dirname + '/../example/src/features/block-scope.js', { entry: true })
+    .bundle(function (err, src) {
+      t.similar(err.message, /Unexpected token let/, 'returns error indicating that let is not supported')
+      t.end();
+    });
+});
