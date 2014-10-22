abfd22c5b3c8d5f5868769a78a70dd7e7e80a1db
Domenic Denicola <domenic@domenicdenicola.com>
2014-10-22 18:33:57 -0400

    Update errors tests for new Traceur

--- test/errors.js
+++ test/errors.js
@@ -11,17 +11,20 @@ test('\nsyntax error', function (t) {
     .transform(es6ify)
     .require(__dirname + '/bundle/syntax-error.js', { entry: true })
     .bundle(function (err, src) {
-      t.ok(err.message.indexOf(path.relative(__dirname, 'bundle/syntax-error.js') + ':1:10: Unexpected token (') === 0);
+      t.similar(err.message, /bundle\/syntax-error\.js\:1\:10\: Unexpected token \(/, 'returns the syntax error');
       t.end();
     });
 })
 
-test('\ncompiling block-scope without blockBinding override', function (t) {
+test('\ncompiling block-scope with blockBinding: false', function (t) {
+  es6ify.traceurOverrides = { blockBinding: false };
+
   browserify()
     .transform(es6ify)
     .require(__dirname + '/../example/src/features/block-scope.js', { entry: true })
     .bundle(function (err, src) {
       t.similar(err.message, /Unexpected token let/, 'returns error indicating that let is not supported')
+      es6ify.traceurOverrides = null;
       t.end();
     });
 });
