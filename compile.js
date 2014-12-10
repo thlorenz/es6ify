533e12b985ecdf5861681b7decc3c7df7a00425f
Arturo Guzman <arturo@kisurfer.com>
2014-12-09 22:04:37 -0500

    Add deprecation warning for traceurOverrides.sourceMap = true

--- compile.js
+++ compile.js
@@ -6,19 +6,20 @@ var Compiler = require('traceur').NodeCompiler
 
 var traceurOptions = {
   modules      : 'commonjs',
-  sourceMaps   : true
+  sourceMaps   : 'inline'
 };
 
 function buildTraceurOptions(overrides) {
   var options = xtend(traceurOptions, overrides);
 
   if (typeof options.sourceMap !== 'undefined') {
-    console.warn('es6ify: DEPRECATED options.sourceMap has changed to options.sourceMaps (plural)');
+    console.warn('es6ify: DEPRECATED traceurOverrides.sourceMap has changed to traceurOverrides.sourceMaps (plural)');
     options.sourceMaps = options.sourceMap;
     delete options.sourceMap;
   }
 
   if (options.sourceMaps === true) {
+    console.warn('es6ify: DEPRECATED "traceurOverrides.sourceMaps = true" is not a valid option, traceur sourceMaps options are [false|inline|file]');
     options.sourceMaps = 'inline';
   }
 
