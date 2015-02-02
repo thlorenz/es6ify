b389bc794f58c5386ff0669cb75477863892b79e
Jesse McCarthy <git_commits@jessemccarthy.net>
2015-02-02 13:42:49 -0500

    Extract to function for use in multiple tests.

--- test/transform.js
+++ test/transform.js
@@ -9,96 +9,99 @@ var test       =  require('tap').test
   , compile    =  require('../compile')
   , proxyquire =  require('proxyquire')
 
+/**
+ * @function
+ * @param {{useSourceRoot: boolean}} opts
+ * useSourceRoot Do or don't specify sourceRoot to traceur.
+ */
+function addsSourceMap (t, opts) {
+  t.plan(3);
+  var data = '';
+  var compiles = 0;
+
+  function trackingCompile() {
+    compiles++;
+    var args = [].slice.call(arguments);
+    return compile.apply(this, args);
+  }
+
+  // Input / output paths
+  var paths = { out: {} };
+
+  paths.in = {
+    sourceRoot: path.join(__dirname, '..', 'example', 'src'),
+    // Path that should wind up in `sources` in the map when sourceRoot is used.
+    sources: path.join('features', 'iterators.js'),
+  };
+  paths.in.file = path.join(paths.in.sourceRoot, paths.in.sources);
+
+  var es6ifyOpts = { sourceRoot: paths.in.sourceRoot };
+  var es6ify = proxyquire('..', { './compile' : trackingCompile } )
+  es6ify = es6ify.configure(es6ifyOpts);
+
+  var contents = fs.readFileSync(paths.in.file, { encoding: 'utf8' });
+
+  // Run without, then with, `end()`
+  [undefined, end].forEach(function (end) {
+    fs.createReadStream(paths.in.file)
+      .pipe(es6ify(paths.in.file))
+      .on('error', console.error)
+      .pipe(through(write, end));
+  });
+
+  function write (buf) { data += buf; }
+  function end () {
+    var sourceMap = convert.fromSource(data).toObject();
+
+    // Traceur converts all \s to /s so we need to do so also before comparing
+    paths.out.file = (es6ifyOpts.sourceRoot ? paths.in.sources : paths.in.file)
+      .replace(/\\/g, '/');
+
+    paths.out.sources = paths.in.sources.replace(/\\/g, '/');
+
+    paths.out.sourceRoot = (
+      es6ifyOpts.sourceRoot ?
+      paths.in.sourceRoot :
+      path.dirname(paths.in.file) + '/'
+    )
+      .replace(/\\/g, '/');
+
+    t.deepEqual(
+        sourceMap
+      , { version: 3,
+          file: paths.out.file,
+          sources: [
+            paths.out.sources,
+            '@traceur/generated/TemplateParser/2',
+            '@traceur/generated/TemplateParser/3',
+          ],
+          names: [],
+          mappings: sourceMap.mappings,
+          sourceRoot: paths.out.sourceRoot,
+          sourcesContent: [
+            contents,
+
+            '\n        for (var $__placeholder__0 =\n' +
+            '                 $__placeholder__1[\n' +
+            '                     $traceurRuntime.toProperty(Symbol.iterator)](),\n' +
+            '                 $__placeholder__2;\n' +
+            '             !($__placeholder__3 = $__placeholder__4.next()).done; ) {\n' +
+            '          $__placeholder__5;\n' +
+            '          $__placeholder__6;\n' +
+            '        }',
+
+            'void 0'
+          ] }
+      , 'adds sourcemap comment including original source'
+    );
+    t.ok(sourceMap.mappings.length);
+    t.equal(compiles, 1, 'compiles only the first time');
+  }
+}
+// addsSourceMap
+
 test('transform adds sourcemap comment and uses cache on second time', function (t) {
-    t.plan(3);
-    var data = '';
-    var compiles = 0;
 
-    function trackingCompile() {
-      compiles++;
-      var args = [].slice.call(arguments);
-      return compile.apply(this, args);
-    }
-
-    // Input / output paths
-    var paths = { out: {} };
-
-    paths.in = {
-      sourceRoot: path.join(__dirname, '..', 'example', 'src'),
-      // Path that should wind up in `sources` in the map when sourceRoot is
-      // used.
-      sources: path.join('features', 'iterators.js'),
-    };
-    paths.in.file = path.join(paths.in.sourceRoot, paths.in.sources);
-
-    var opts = { sourceRoot: sourceRoot };
-
-    if (opts.sourceRoot === undefined) {
-      paths.in.sources = path.basename(paths.in.file);
-    }
-
-    var es6ify = proxyquire('..', { './compile' : trackingCompile } )
-
-    es6ify = es6ify.configure(opts);
-
-    var contents = fs.readFileSync(paths.in.file, { encoding: 'utf8' });
-
-    // Run without, then with, `end()`
-    [undefined, end].forEach(function (end) {
-      fs.createReadStream(paths.in.file)
-        .pipe(es6ify(paths.in.file))
-        .on('error', console.error)
-        .pipe(through(write, end));
-    });
-
-    function write (buf) { data += buf; }
-    function end () {
-      var sourceMap = convert.fromSource(data).toObject();
-
-      // Traceur converts all \s to /s so we need to do so also before comparing
-      paths.out.file = (opts.sourceRoot ? paths.in.sources : paths.in.file)
-        .replace(/\\/g, '/');
-
-      paths.out.sources = paths.in.sources.replace(/\\/g, '/');
-
-      paths.out.sourceRoot = (
-        opts.sourceRoot ?
-        paths.in.sourceRoot :
-        path.dirname(paths.in.file) + '/'
-      )
-        .replace(/\\/g, '/');
-
-      t.deepEqual(
-          sourceMap
-        , { version: 3,
-            file: paths.out.file,
-            sources: [
-              paths.out.sources,
-              '@traceur/generated/TemplateParser/2',
-              '@traceur/generated/TemplateParser/3',
-            ],
-            names: [],
-            mappings: sourceMap.mappings,
-            sourceRoot: paths.out.sourceRoot,
-            sourcesContent: [
-              contents,
-
-              '\n        for (var $__placeholder__0 =\n' +
-              '                 $__placeholder__1[\n' +
-              '                     $traceurRuntime.toProperty(Symbol.iterator)](),\n' +
-              '                 $__placeholder__2;\n' +
-              '             !($__placeholder__3 = $__placeholder__4.next()).done; ) {\n' +
-              '          $__placeholder__5;\n' +
-              '          $__placeholder__6;\n' +
-              '        }',
-
-              'void 0'
-            ] }
-        , 'adds sourcemap comment including original source'
-      );
-      t.ok(sourceMap.mappings.length);
-      t.equal(compiles, 1, 'compiles only the first time');
-    }
 });
 
 test('transform does not add sourcemaps if traceurOverrides.sourceMaps is false', function (t) {
